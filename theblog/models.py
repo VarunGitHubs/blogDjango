4eb39acd92738ccf16c889fb661958d09b8f7488
Varun Srinivasan <varun.srijayvar@gmail.com>
2023-02-20 19:07:00 +0000

    final changes

--- theblog/models.py
+++ theblog/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
 class Comment(models.Model):
 	post = models.ForeignKey(Post,related_name = 'comments', on_delete = models.CASCADE)
 	name = models.CharField(max_length = 255)
-	body = models.TextField()
+	body = models.CharField (max_length = 255)
 	date_added = models.DateTimeField(auto_now_add = True)
 
 	def __str__(self):
