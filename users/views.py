243facee54309ac905c4e1b2527bc293d05ba3d4
Varun Srinivasan <varun.srijayvar@gmail.com>
2023-03-01 20:00:09 +0000

    HOPEFULLY I HAVE REMOVED CSRF

--- users/views.py
+++ users/views.py
@@ -3,9 +3,11 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse, reverse_lazy
 from .forms import RegisterForm
-# Create your views here.
+from braces.views import  CsrfExemptMixin
 
-class UserRegisterView(generic.CreateView):
+
+# Create your views here.
+class UserRegisterView(CsrfExemptMixin, generic.CreateView):
 	form_class = RegisterForm # Initialise registation form 
 	#form_class = UserCreationForm
 	template_name = 'registration/register.html' # Specify the HTML page to view
